5f5218f8c3c5ec5bd7eaf8186e3051a326a23a71
C. Vishwanath <87000253+c-viswanath@users.noreply.github.com>
2022-06-30 21:13:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+function displayTime(){
+    let dateTime = new Date();
+
+    var hrsIN = dateTime.getHours();
+    let minsIN = dateTime.getMinutes();
+    let secIN = dateTime.getSeconds();
+    let sessionIN = document.querySelector('#sessionIN');
+
+    let hrsUS= hrsIN-9;
+    let minsUS;
+    if (minsIN>=30){
+        minsUS= minsIN-30;
+    }else{
+        hrsUS=hrsUS-1;
+        minsUS=30+minsIN;
+    }
+
+    let secUS=secIN;
+    let sessionUS = document.querySelector('sessionUS');
+
+
+
+    if(hrsIN>=12){
+        sessionIN.innerHTML ='PM';
+    }else{
+        sessionIN.innerHTML ='AM';
+    }
+
+
+
+    document.querySelector('#hoursIN').innerHTML =hrsIN;
+    document.querySelector('#minutesIN').innerHTML =minsIN;
+    document.querySelector('#secondsIN').innerHTML =secIN;
+
+    document.querySelector('#hoursUS').innerHTML =hrsUS;
+    document.querySelector('#minutesUS').innerHTML =minsUS;
+    document.querySelector('#secondsUS').innerHTML =secUS;
+
+}
+setInterval(displayTime , 10);
